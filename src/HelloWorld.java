86a106f82e3ea5021945df359f0f8ee94a03d886
Saltanat2020 <saabdubek@gmail.com>
2020-11-10 12:20:39 -0500

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi, B21!");
+        System.out.println("Have a good day!");
     }
 }
