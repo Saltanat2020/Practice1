74953441c4c48ba79e11791b06de45a7f36c1515
Saltanat2020 <saabdubek@gmail.com>
2020-11-10 14:43:00 -0500

    commit

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,6 +1,8 @@
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Practice is useful");
+// more stuff
+// Mac user is happy to see commit dialog finally
 
     }
 }
